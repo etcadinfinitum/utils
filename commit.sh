9074db0744a0dd3b7c5c3c841bc3036405e6ff45
Lizzy Presland <lizzy.presland@gmail.com>
2019-06-05 01:48:10 -0700

    Added script control to allow commit amending as an I/O prompt For...convenience. Also cleaned up whitespace and added a few commonts about changes.

--- commit.sh
+++ commit.sh
@@ -4,12 +4,27 @@ cp ~/.bashrc .
 cp ~/.vimrc .
 cp ~/vpn.sh .
 cp ~/lab.sh .
-git diff .bashrc
-git diff .vimrc
-echo -e "\n\nRunning git commit now; press enter to continue"
-read tmp
-git add --all
-git commit
+git diff
+
+echo -e "\n\nRunning git commit now"
+
+# give a nice prompt to do commit vs commit --amend
+echo "Enter 1 to do a fresh commit, 2 to do 'commit --amend': "
+read amend
+
+# use read value to do commit
+if [[ $amend == "1" ]]; then
+    git add --all
+    git commit
+elif [[ $amend == "2" ]]; then
+    git add --all
+    git commit --amend
+else
+    echo "Invalid option provided. Aborting commit."
+    # git reset HEAD *
+    # exit 0
+fi
+
 rm .bashrc
 rm .vimrc
 rm vpn.sh
