bc0f20135c38b4c4eadc89e97f9b88e467e72789
Lizzy Presland <lizzy.presland@gmail.com>
2019-06-05 19:29:16 -0700

    added a deployment script to deploy to new machine(s)

---
+++ deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo "Deploying .bashrc"
+if [[ -f ~/.bashrc ]]; then
+    mv ~/.bashrc ~/.bashrc_OLD
+fi
+cp ./.bashrc ~/.bashrc
+source ~/.bashrc
+
+echo "Deploying .vimrc"
+if [[ -f ~/.vimrc ]]; then
+    mv ~/.vimrc ~/.vimrc_OLD
+fi
+cp ./.vimrc ~/.vimrc
+
+if [[ `which vim | wc -l` == 0 ]]; then
+    echo "!!! WARNING !!!"
+    echo -e "\tvim is not installed. Consider installing it using apt or via your preferred package manager."
+fi
+
+if [[ `which perl | wc -l` == 0 ]]; then
+    echo "!!! WARNING !!!"
+    echo -e "\tperl is not installed."
+else
+    echo "Terminal colors:"
+    perl 256color.pl
+fi
+
+exit 0
